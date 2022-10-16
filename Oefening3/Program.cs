f4ebd8b7612c0da3c441ccce070fc99e30ffd5bc
LAPTOP-A515C7J8\Sam <bramtrainer@gmail.com>
2022-10-17 00:57:15 +0200

    Ik push de versie van oefening 3 die ik lokaal heb staan. Deze had ik blijkbaar niet naar mijn repository gepushd.

--- Oefening3/Program.cs
+++ Oefening3/Program.cs
@@ -1,17 +1,35 @@
-namespace Oefening3
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+
+/*Ik declareer de variabele teller.
+ Dit is een integer.*/
+int teller = 0;
+
+/*Ik gebruik een while loop.
+ Deze while loop blijft lopen zolang de variabele 'teller' kleiner is dan 100.*/
+while (teller < 100)
 {
-    internal static class Program
+    /*Ik gebruik een if statement.
+     Wanneer de vermenigvuldiging van teller met 7,
+    kleiner is dan 546, breken we uit de if statement.*/
+    if ((teller * 7) > 546)
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
+        /*Ik breek uit de controlestructuur.*/
+        break;
+    }
+
+    /*Als het vorige statement niet het geval is,
+     wordt teller geincriment met 1.
+    In dit geval wordt het getal ook in de console vertoond.*/
+    else
+    {
+        Console.WriteLine(teller);
+        teller++;
     }
 }
+
+/*Door Console.ReadLine() te gebruiken,
+ sluit de console niet vanzelf.
+De gebruiker moet eerst één toets indrukken alvorens deze wordt gesloten.*/
+Console.ReadLine();
